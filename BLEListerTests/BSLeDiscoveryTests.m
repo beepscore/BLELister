a7af8cea4c2da913f2772f0e31a024aae6613e90
beepscore <support@beepscore.com>
2013-09-28 00:01:02 -0700

    Add testSH_waitForTimeInterval.
    TODO: Fix project or workspace so that asynchronous test passes.

--- BLEListerTests/BSLeDiscoveryTests.m
+++ BLEListerTests/BSLeDiscoveryTests.m
@@ -7,6 +7,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "SHTestCaseAdditions.h"
 #import "BSLeDiscovery.h"
 #import "BSLeDiscovery_Private.h"
 
@@ -65,6 +66,19 @@
                 @"expected foundPeripherals nil");
     XCTAssertNil(self.bsLeDiscovery.connectedServices,
                 @"expected connectedServices nil");
+}
+
+-(void)testSH_waitForTimeInterval; {
+    __block BOOL assertion = NO;
+
+    double delayInSeconds = 5.0;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        assertion = YES;
+    });
+
+    [self SH_waitForTimeInterval:delayInSeconds];
+    XCTAssertTrue(assertion, @"expected assertion true");
 }
 
 @end
