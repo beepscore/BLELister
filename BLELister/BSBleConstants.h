a00bc180c06df341378fc64e50b45ef9f2555f6d
beepscore <support@beepscore.com>
2013-09-29 19:33:35 -0700

    Add BSBleConstants.h and m.

---
+++ BLELister/BSBleConstants.h
@@ -0,0 +1,13 @@
+//
+//  BSBleConstants.h
+//  BLELister
+//
+//  Created by Steve Baker on 9/29/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSBleConstants : NSObject
+
+@end
---
+++ BLELister/BSBleConstants.m
@@ -0,0 +1,13 @@
+//
+//  BSBleConstants.m
+//  BLELister
+//
+//  Created by Steve Baker on 9/29/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "BSBleConstants.h"
+
+@implementation BSBleConstants
+
+@end
