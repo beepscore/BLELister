19d1d3c63f28437f4c7763d3764d8c23731f1e4a
beepscore <support@beepscore.com>
2013-09-30 00:05:46 -0700

    In BSJSONParser.h add comments.

--- BLELister/BSJSONParser.h
+++ BLELister/BSJSONParser.h
@@ -10,12 +10,32 @@
 
 @interface BSJSONParser : NSObject
 
+
+/**
+ @return an empty array if argument is nil
+ */
 + (NSArray *)arrayFromJSON:(NSString *)aJSON;
+
+/**
+ @param aJSONFileName is first part of file name, omitting .json extension
+ @return an empty array if argument is nil
+ */
 + (NSArray *)arrayFromJSONFile:(NSString *)aJSONFileName;
 
+/**
+ @return an empty dictionary if argument is nil
+ */
 + (NSDictionary *)dictFromJSON:(NSString *)aJSON;
+
+/**
+ @param aJSONFileName is first part of file name, omitting .json extension
+ @return an empty dictionary if argument is nil
+ */
 + (NSDictionary *)dictFromJSONFile:(NSString *)aJSONFileName;
 
+/**
+ @param aJSONFileName is first part of file name, omitting .json extension
+ */
 + (NSString *)JSONStringFromFile:(NSString *)aJSONFileName;
 
 @end
