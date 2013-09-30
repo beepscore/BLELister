f40bd3b5b2624c7e1b50a8fcd3999e9b1af7bb7c
beepscore <support@beepscore.com>
2013-09-29 22:02:03 -0700

    Add BSJSONParserTests.

---
+++ BLEListerTests/BSJSONParserTests.m
@@ -0,0 +1,34 @@
+//
+//  BSJSONParserTests.m
+//  BLELister
+//
+//  Created by Steve Baker on 9/29/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BSJSONParserTests : XCTestCase
+
+@end
+
+@implementation BSJSONParserTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
