a76d0493c2b6fe503a4aa47bfb448f53b9edaf80
beepscore <support@beepscore.com>
2013-10-09 17:06:45 -0700

    In BSMasterViewControllerTests add assertion, log statements, revise comment.

--- BLEListerTests/BSMasterViewControllerTests.m
+++ BLEListerTests/BSMasterViewControllerTests.m
@@ -8,6 +8,7 @@
 
 #import <XCTest/XCTest.h>
 #import "BSMasterViewController.h"
+#import "BSLeDiscovery.h"
 
 @interface BSMasterViewControllerTests : XCTestCase
 
@@ -30,11 +31,16 @@
 - (void)testViewDidLoadSetsLeDiscovery
 {
     BSMasterViewController *vc = [[BSMasterViewController alloc] init];
+    XCTAssertNil(vc.leDiscovery, @"expected leDiscovery nil");
     [vc viewDidLoad];
     XCTAssertNotNil(vc.leDiscovery, @"expected leDiscovery");
 
-    // FIXME: this fails, 2 different objects.
-    // sharedInstance dispatchOnce is getting called twice!
+    // Currently this returns 2 different objects and test fails.
+    // TODO: Investigate why.
+    NSLog(@"***sharedInstance %@", (BSLeDiscovery *)[BSLeDiscovery sharedInstance]);
+    NSLog(@"***vc.leDiscovery %@", vc.leDiscovery);
+    NSLog(@"***sharedInstance.centralManager %@", [(BSLeDiscovery *)[BSLeDiscovery sharedInstance] centralManager]);
+    NSLog(@"***vc.leDiscovery.centralManager %@", vc.leDiscovery.centralManager);
     //XCTAssertEqualObjects([BSLeDiscovery sharedInstance], vc.leDiscovery, @"expected leDiscovery");
 }
 
