4877f29fce53b82a80954fe407d2f3323c444935
beepscore <support@beepscore.com>
2013-10-12 03:26:12 -0700

    Add BSStubCBCentralManager.
    BSStubCBCentralManager stubs a minimal subset of CBCentralManager for use by unit tests.
    I found this amount by running unit tests with the stub, and adding enough to get tests to pass.
    These tests will be committed in following commit.
    Reference
    http://martinfowler.com/articles/mocksArentStubs.html

---
+++ BLEListerTests/BSStubCBCentralManager.m
@@ -0,0 +1,18 @@
+//
+//  BSStubCBCentralManager.m
+//  BLELister
+//
+//  Created by Steve Baker on 10/12/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "BSStubCBCentralManager.h"
+
+@implementation BSStubCBCentralManager
+
+- (NSArray *)retrieveConnectedPeripheralsWithServices:(NSArray *)serviceUUIDs
+{
+    return nil;
+}
+
+@end
