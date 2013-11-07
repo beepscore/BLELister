3c22530069f37d70942806201929693153129985
beepscore <support@beepscore.com>
2013-11-06 16:14:27 -0800

    Add BSDetailViewController_Private.h with property notificationCenter.

---
+++ BLELister/Controllers/BSDetailViewController_Private.h
@@ -0,0 +1,17 @@
+//
+//  BSDetailViewController_Private.h
+//  BLELister
+//
+//  Created by Steve Baker on 10/11/13.
+//  Copyright (c) 2013 Beepscore LLC. All rights reserved.
+//
+
+#import "BSDetailViewController.h"
+
+@interface BSDetailViewController () {
+}
+
+// viewDidLoad sets self.notificationCenter. Unit test can set it to a mock notificationCenter.
+@property (nonatomic, strong) NSNotificationCenter *notificationCenter;
+
+@end
