eb48ac5ad0e947001f18c25ab79fdd5c16f9564f
beepscore <support@beepscore.com>
2013-11-08 13:49:12 -0800

    In CBCentralManagerTests revise tests.

--- BLEListerTests/CBCentralManagerTests.m
+++ BLEListerTests/CBCentralManagerTests.m
@@ -11,6 +11,9 @@
 #import "SHTestCaseAdditions.h"
 #import <CoreBluetooth/CoreBluetooth.h>
 #import "BSBleConstants.h"
+#import "BSJSONParser.h"
+#import "BSLeDiscovery.h"
+#import "BSLeDiscovery_Private.h"
 
 @interface CBCentralManagerTests : XCTestCase
 @property (strong, nonatomic) CBCentralManager *centralManager;
@@ -30,25 +33,80 @@
     [super tearDown];
 }
 
+- (BOOL)isNowBefore:(NSDate *)aDate
+{
+    return (NSOrderedDescending == [[NSDate date] compare:aDate]);
+}
+
 #pragma mark -
+// This test fails
+// I think Xcode may be running app before starting tests.
+// Even if I choose to run only this test,
+// Xcode creates a [BSLeDiscovery sharedInstance] with a centralManager.
+// Test creates a central manager, but it doesn't become powered on.
+// Possible causes:
+// Could be because only one centralManager can be poweredOn,
+// Could be because test runs in background and iOS won't let it power on.
+// If centralManager used by tests isn't powered on, then scans won't work.
+// If test references [BSLeDiscovery sharedInstance] it gets a second one.
+// Possible fixes:
+// Might be able to change unit test scheme to not build app first.
+// Might be able to make initial [BSLeDiscovery sharedInstance] available to test.
+/*
 - (void)testState {
+    //self.centralManager = [[BSLeDiscovery sharedInstance] centralManager];
+
+    self.centralManager = [[CBCentralManager alloc] initWithDelegate:nil
+                                                                queue:nil];
+    NSLog(@"%@ state: %d", self.centralManager,
+          (int)self.centralManager.state);
+
+    NSDate *timeoutDate = [NSDate dateWithTimeIntervalSinceNow:15];
+
+    while ( (CBCentralManagerStatePoweredOn != self.centralManager.state)
+           && ![self isNowBefore:timeoutDate] ) {
+        NSLog(@"%@ state: %d", self.centralManager,
+              (int)self.centralManager.state);
+        sleep(1);
+    }
+    XCTAssertEqual(CBCentralManagerStatePoweredOn, self.centralManager.state,
+                     @"expected centralManager state on");
+}
+*/
+
+- (void)findAndTestPeripheral:(NSString *)peripheralKey {
+    
     self.centralManager = [[CBCentralManager alloc]
                            initWithDelegate:nil
                            queue:nil];
-    
-    // centralManager state varies.
-    
-    NSLog(@"centralManager state: %d", (int)self.centralManager.state);
-    
-    BOOL isValidManagerState = ( (CBCentralManagerStateUnknown == self.centralManager.state)
-                                || (CBCentralManagerStateResetting == self.centralManager.state)
-                                || (CBCentralManagerStateUnsupported == self.centralManager.state)
-                                || (CBCentralManagerStateUnauthorized == self.centralManager.state)
-                                || (CBCentralManagerStatePoweredOff == self.centralManager.state)
-                                || (CBCentralManagerStatePoweredOn == self.centralManager.state)
-                                );
-    
-    XCTAssertTrue(isValidManagerState, @"expected centralManager state valid");
+
+    NSDictionary *bleDevices = [BSJSONParser dictFromJSONFile:@"bleDevices"];
+    NSString *expectedIdentifier = bleDevices[peripheralKey][@"identifier"];
+    NSString *expectedName = bleDevices[peripheralKey][@"name"];
+
+    NSArray *peripheralsWithIdentifiers = [self.centralManager retrievePeripheralsWithIdentifiers:@[expectedIdentifier]];
+
+    // TODO: add retry loop if array is empty, then exit or timeout and assert
+    NSLog(@"peripheralsWithIdentifiers %@", peripheralsWithIdentifiers);
+    XCTAssertNotNil(peripheralsWithIdentifiers, @"");
+    XCTAssertTrue((0 <= [peripheralsWithIdentifiers count]),
+                  @"expected peripheralsWithIdentifiers has 0 or more objects");
+
+    CBPeripheral *peripheral = [peripheralsWithIdentifiers firstObject];
+
+    XCTAssertEqualObjects(expectedIdentifier,
+                          [peripheral.identifier UUIDString],
+                          @"expected first found peripheral UUIDString");
+    XCTAssertEqualObjects(expectedName,
+                          peripheral.name,
+                          @"expected first found peripheral name");
+}
+
+// testFoundPeripheralBLEShield requires an Arduino with RedBearLab BLE shield
+// within range of the iOS device.
+// It assumes BLE shield with be the first device found.
+- (void)testFoundPeripheralBLEShield {
+    [self findAndTestPeripheral:@"redbearshield"];
 }
 
 @end
