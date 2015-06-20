b5803e124b9799c89677dafcf331faa550993dda
beepscore <support@beepscore.com>
2015-06-20 11:43:44 -0700

    Add MVC Model class BSBlePeripheral

---
+++ BLELister/Model/BSBlePeripheral.h
@@ -0,0 +1,13 @@
+//
+//  BSBlePeripheral.h
+//  
+//
+//  Created by Steve Baker on 6/20/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BSBlePeripheral : NSObject
+
+@end
---
+++ BLELister/Model/BSBlePeripheral.m
@@ -0,0 +1,13 @@
+//
+//  BSBlePeripheral.m
+//  
+//
+//  Created by Steve Baker on 6/20/15.
+//
+//
+
+#import "BSBlePeripheral.h"
+
+@implementation BSBlePeripheral
+
+@end
