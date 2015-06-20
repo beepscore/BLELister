fd8818a885d3852e8ba802ef8752eb76075b9a24
beepscore <support@beepscore.com>
2015-06-20 12:01:17 -0700

    In BSBlePeripheral.h add doc comments and add properties peripheral and RSSI

--- BLELister/Model/BSBlePeripheral.h
+++ BLELister/Model/BSBlePeripheral.h
@@ -7,7 +7,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
 
+/**
+ * BSBlePeripheral is composed of a CBPeripheral and an RSSI.
+ * This object can be used to replace
+ * CBPeripheral method [peripheral RSSI], deprecated in iOS 8
+ */
 @interface BSBlePeripheral : NSObject
-
+@property (nonatomic, strong) CBPeripheral *peripheral;
+@property (nonatomic, strong) NSNumber *RSSI;
 @end
