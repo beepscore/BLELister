19a9e3c5cb8eaf67109f142d12f4379480e300ff
beepscore <support@beepscore.com>
2017-05-10 23:08:47 -0700

    In BSMasterViewController delete commented out methods. These methods are not present in MasterViewController.

--- BLELister/Controllers/BSMasterViewController.m
+++ BLELister/Controllers/BSMasterViewController.m
@@ -132,22 +132,6 @@ forRowAtIndexPath:(NSIndexPath *)indexPath {
     }
 }
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 - (void)tableView:(UITableView *)tableView
 didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
