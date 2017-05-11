bac28124183113456a26fc5fbe6ff5f1858f468b
beepscore <support@beepscore.com>
2017-05-11 10:41:21 -0700

    Delete commented out conditional

--- BLELister/Controllers/MasterViewController.m
+++ BLELister/Controllers/MasterViewController.m
@@ -137,10 +137,8 @@
 // TODO: I think this isn't needed with new master-detail. Double check.
 - (void)tableView:(UITableView *)tableView
 didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    //if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-        BSBlePeripheral *bsBlePeripheral = self.leDiscovery.foundPeripherals[indexPath.row];
-        self.detailViewController.detailItem = bsBlePeripheral;
-    //}
+    BSBlePeripheral *bsBlePeripheral = self.leDiscovery.foundPeripherals[indexPath.row];
+    self.detailViewController.detailItem = bsBlePeripheral;
 }
 
 #pragma mark - Register for notifications
